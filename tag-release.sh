441550a5c6eba6315435eb281116d38ae9a1bb1a
James Ketrenos <james.p.ketrenos@intel.com>
2017-06-23 09:43:52 -0700

    Added tag-release.sh
    Signed-off-by: James Ketrenos <james.p.ketrenos@intel.com>

---
+++ tag-release.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Get the highest tag number from GIT
+
+VERSION=$(git tag | sort -rV | head -n 1)
+
+# Strip the v
+VERSION=${VERSION:1}
+VERSION="${VERSION%.*}.$((${VERSION##*.}+1))"
+
+TAG=${1}
+TAG=${TAG:=${VERSION}}
+
+[ "${1}" == "--help" ] && {
+cat << EOF
+
+usage: tag-release.sh [major.api.minor]
+
+Example:
+
+./tag-releas.sh 0.0.9
+
+If version string is not provided, the next highest tag is calculated
+based on the tags in git, which is '${VERSION}'.
+
+EOF
+  exit -1
+}
+
+
+git diff-index --quiet --cached HEAD -- || {
+cat << EOF
+
+Staged (not yet committed) changes detected in tree. You must
+commit or stash the changes prior to running ./tag-release.sh
+
+EOF
+  exit -1
+}
+
+git tag -a v${TAG} -m "${TAG}"
+git push --tags
+
